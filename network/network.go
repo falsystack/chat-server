8a3730a7bab628e2bd2ba985f9651d327871ef0f
falsystack <falsystack@gmail.com>
2024-07-29 00:29:22 +0900

    feat: Add socket

--- network/network.go
+++ network/network.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
 )
@@ -10,7 +11,18 @@ type Network struct {
 }
 
 func NewServer() *Network {
-	return &Network{engin: gin.New()}
+	n := &Network{engin: gin.New()}
+
+	n.engin.Use(gin.Logger())   // user logger
+	n.engin.Use(gin.Recovery()) // panic 또는 에러로 인한 서버가 죽으면 다시 기동시켜주는 역할을 한다
+	n.engin.Use(cors.New(cors.Config{
+		AllowWebSockets: true,
+		AllowOrigins:    []string{"*"},
+		AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:    []string{"*"},
+	}))
+
+	return n
 }
 
 func (n *Network) StartServer() error {
