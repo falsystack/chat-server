b07074cb31e58cae215fb1b188eb0c1b00ab76c8
falsystack <falsystack@gmail.com>
2024-07-29 00:44:02 +0900

    feat: Add websocket connect method

--- network/socket.go
+++ network/socket.go
@@ -1,12 +1,26 @@
 package network
 
-import "golang.org/x/net/websocket"
+import (
+	. "chat-server/types"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+// http -> websocket 으로 업그레이드
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  SocketBufferSize,
+	WriteBufferSize: MessageBufferSize,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 type Room struct {
 	Forward chan *message    // 수신되는 메세지를 보관
-	Join    chan *Client     // Socket 이 연결되는 경우에 작동
-	Leave   chan *Client     // Socket 이 끊어지는 경우에 작동
-	Clients map[*Client]bool // 현재 방에 있는 Client 정보를 저장
+	Join    chan *client     // Socket 이 연결되는 경우에 작동
+	Leave   chan *client     // Socket 이 끊어지는 경우에 작동
+	Clients map[*client]bool // 현재 방에 있는 client 정보를 저장
 }
 
 type message struct {
@@ -15,9 +29,43 @@ type message struct {
 	Time    int64
 }
 
-type Client struct {
+type client struct {
 	Send   chan *message
 	Room   *Room
 	Name   string
 	Socket *websocket.Conn
+}
+
+func NewRoom() *Room {
+	return &Room{
+		Forward: make(chan *message),
+		Join:    make(chan *client),
+		Leave:   make(chan *client),
+		Clients: make(map[*client]bool),
+	}
+}
+
+func (r *Room) SocketServe(c *gin.Context) {
+	socket, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	userCookie, err := c.Request.Cookie("auth")
+	if err != nil {
+		panic(err)
+	}
+
+	client := &client{
+		Socket: socket,
+		Send:   make(chan *message, MessageBufferSize),
+		Room:   r,
+		Name:   userCookie.Value,
+	}
+
+	r.Join <- client
+
+	defer func() {
+		r.Leave <- client
+	}()
 }
